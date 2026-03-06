7843d5e91ef83ad89a9b1720a78c9ec723f227a1
Arthur Nishikawa <nishikawa.poli@gmail.com>
2026-03-06 21:40:27 +0100

    Add handling of errors with venv setup

--- plugins/venv-prompt/venv-prompt.plugin.zsh
+++ plugins/venv-prompt/venv-prompt.plugin.zsh
@@ -1,29 +1,39 @@
 # Function to update the prompt when a virtual environment is activated/deactivated
 update_virtual_env_prompt() {
-    which python &>/dev/null
-    [[ $? -ne 0 ]] && return
-
     # If VIRTUAL_ENV_DISABLE_PROMPT is set, don't modify the prompt
     ! [ -z "${VIRTUAL_ENV_DISABLE_PROMPT+_}" ] && return
 
     if ! [ -z "${VIRTUAL_ENV+_}" ] && ! [ -z "${VIRTUAL_ENV_PROMPT+_}" ]; then
+        unset error_msg
+
+        python_exe=$(which python)
+        if [[ $? -ne 0 ]]; then
+            error_msg="Error! Python not found"
+        elif ! [[ "$python_exe" == "$VIRTUAL_ENV"* ]]; then
+            error_msg="Error! Python path doesn't match set VIRTUAL_VENV"
+        fi
+
         # Save the original PS1 if not already saved
         if [[ -z $_OLD_VIRTUAL_PS1 ]]; then
             _OLD_VIRTUAL_PS1="${PS1-}"
         fi
 
-        # Get Python version from the virtual environment
-        local python_version=$(python --version 2>&1 | cut -d' ' -f2)
+        if [[ -z "${error_msg+_}" ]]; then
+            # Get Python version from the virtual environment
+            python_version=$(python --version 2>&1 | cut -d' ' -f2)
 
-        # Extract the environment name without parentheses and create a new prompt
-        local venv_name=${VIRTUAL_ENV_PROMPT//[()]/}
-        venv_name=${venv_name%% *} # Remove any trailing spaces
+            # Extract the environment name without parentheses and create a new prompt
+            venv_name=${VIRTUAL_ENV_PROMPT//[()]/}
+            venv_name=${venv_name%% *} # Remove any trailing spaces
 
-        # Create custom prompt with Python logo
-        local custom_prompt="($venv_name $python_version) "
+            custom_prompt="($venv_name $python_version)"
 
-        # Set custom prompt
-        PS1="$custom_prompt${_OLD_VIRTUAL_PS1-}"
+            # Set custom prompt
+        else
+            custom_prompt="($error_msg)"
+        fi
+
+        PS1="$custom_prompt ${_OLD_VIRTUAL_PS1-}"
     elif ! [ -z "${_OLD_VIRTUAL_PS1+_}" ]; then
         # Restore original PS1 when deactivated
         PS1="$_OLD_VIRTUAL_PS1"
