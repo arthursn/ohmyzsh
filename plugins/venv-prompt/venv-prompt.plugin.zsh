d4fb60639d1f55a7cfa6493f8f3eff384787b730
Arthur Nishikawa <nishikawa.poli@gmail.com>
2026-03-07 12:43:28 +0100

    Improve logic

--- plugins/venv-prompt/venv-prompt.plugin.zsh
+++ plugins/venv-prompt/venv-prompt.plugin.zsh
@@ -1,39 +1,42 @@
 # Function to update the prompt when a virtual environment is activated/deactivated
 update_virtual_env_prompt() {
-    # If VIRTUAL_ENV_DISABLE_PROMPT is set, don't modify the prompt
-    ! [ -z "${VIRTUAL_ENV_DISABLE_PROMPT+_}" ] && return
-
     if ! [ -z "${VIRTUAL_ENV+_}" ] && ! [ -z "${VIRTUAL_ENV_PROMPT+_}" ]; then
-        unset error_msg
-
-        python_exe=$(which python)
-        if [[ $? -ne 0 ]]; then
-            error_msg="Error! Python not found"
-        elif ! [[ "$python_exe" == "$VIRTUAL_ENV"* ]]; then
-            error_msg="Error! Python path doesn't match set VIRTUAL_VENV"
+        if ! [ -z "${VIRTUAL_ENV_DISABLE_PROMPT+_}" ]; then
+            # If VIRTUAL_ENV_DISABLE_PROMPT is set, reset to the original prompt only if _OLD_VIRTUAL_PS1 is set
+            if ! [ -z $_OLD_VIRTUAL_PS1 ]; then
+                PS1="$_OLD_VIRTUAL_PS1"
+            fi
+            return
         fi
 
         # Save the original PS1 if not already saved
-        if [[ -z $_OLD_VIRTUAL_PS1 ]]; then
+        if [ -z $_OLD_VIRTUAL_PS1 ]; then
             _OLD_VIRTUAL_PS1="${PS1-}"
         fi
 
-        if [[ -z "${error_msg+_}" ]]; then
+        local prompt_prefix
+        local python_exe
+
+        # Exit code will determine whether python executable exists or not
+        python_exe=$(which python)
+        
+        if [ $? -ne 0 ]; then
+            prompt_prefix="(Error! Python not found) "
+        elif ! [[ "$python_exe" == "$VIRTUAL_ENV"* ]]; then
+            prompt_prefix="(Error! Python path doesn't match set VIRTUAL_VENV) "
+        else
             # Get Python version from the virtual environment
-            python_version=$(python --version 2>&1 | cut -d' ' -f2)
+            local python_version=$(python --version 2>&1 | cut -d' ' -f2)
 
             # Extract the environment name without parentheses and create a new prompt
-            venv_name=${VIRTUAL_ENV_PROMPT//[()]/}
+            local venv_name=${VIRTUAL_ENV_PROMPT//[()]/}
             venv_name=${venv_name%% *} # Remove any trailing spaces
 
-            custom_prompt="($venv_name $python_version)"
-
-            # Set custom prompt
-        else
-            custom_prompt="($error_msg)"
+            prompt_prefix="($venv_name $python_version) "
         fi
 
-        PS1="$custom_prompt ${_OLD_VIRTUAL_PS1-}"
+        # Set custom prompt
+        PS1="$prompt_prefix${_OLD_VIRTUAL_PS1-}"
     elif ! [ -z "${_OLD_VIRTUAL_PS1+_}" ]; then
         # Restore original PS1 when deactivated
         PS1="$_OLD_VIRTUAL_PS1"
