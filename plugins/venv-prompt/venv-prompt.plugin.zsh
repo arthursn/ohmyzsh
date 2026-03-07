51132f6338b898ae8b432b78f6ab4e980b5b5b43
Arthur Nishikawa <nishikawa.poli@gmail.com>
2026-03-07 22:35:21 +0100

    Improve logic

--- plugins/venv-prompt/venv-prompt.plugin.zsh
+++ plugins/venv-prompt/venv-prompt.plugin.zsh
@@ -1,54 +1,48 @@
 # Function to update the prompt when a virtual environment is activated/deactivated
 update_virtual_env_prompt() {
-    if ! [ -z "${VIRTUAL_ENV+_}" ] && ! [ -z "${VIRTUAL_ENV_PROMPT+_}" ]; then
-        if ! [ -z "${VIRTUAL_ENV_DISABLE_PROMPT+_}" ]; then
-            # If VIRTUAL_ENV_DISABLE_PROMPT is set, reset to the original prompt only if _OLD_VIRTUAL_PS1 is set
-            if ! [ -z $_OLD_VIRTUAL_PS1 ]; then
-                PS1="$_OLD_VIRTUAL_PS1"
-            fi
-            return
-        fi
-
-        # Save the original PS1 if not already saved
-        if [ -z $_OLD_VIRTUAL_PS1 ]; then
-            _OLD_VIRTUAL_PS1="${PS1-}"
-        fi
-
-        local prompt_python_msg
-        local python_exe
-
-        # Exit code will determine whether python executable exists or not
-        python_exe=$(which python)
-
-        if [ $? -ne 0 ]; then
-            prompt_python_msg="Error! Python not found"
-        elif ! [[ "$python_exe" == "$VIRTUAL_ENV"* ]]; then
-            prompt_python_msg="Error! Python executable isn't in VIRTUAL_VENV"
-        else
-            # Get Python version from the virtual environment
-            local python_version=$(python --version 2>&1 | cut -d' ' -f2)
-            python_version=${python_version//$'\r'/} # Remove carriage return
-
-            # Extract the environment name without parentheses and create a new prompt
-            local venv_name=${VIRTUAL_ENV_PROMPT//[()]/}
-            venv_name=${venv_name%% *} # Remove any trailing spaces
-
-            prompt_python_msg="$venv_name $python_version"
-        fi
-
-        local python_icon
-        if ! [ -z ${OMZ_PYTHON_ICON+_} ]; then
-            python_icon="$OMZ_PYTHON_ICON "
-        fi
-        local prompt_prefix="($python_icon$prompt_python_msg) "
-
-        # Set custom prompt
-        PS1="$prompt_prefix${_OLD_VIRTUAL_PS1-}"
-    elif ! [ -z "${_OLD_VIRTUAL_PS1+_}" ]; then
-        # Restore original PS1 when deactivated
-        PS1="$_OLD_VIRTUAL_PS1"
-        unset _OLD_VIRTUAL_PS1
+    # If any of the following variables are unset, there is nothing to do
+    if [ -z "${VIRTUAL_ENV+_}" ] ||
+        [ -z "${VIRTUAL_ENV_PROMPT+_}" ] ||
+        [ -z "{$_OLD_VIRTUAL_PS1+_}" ]; then
+        return
     fi
+
+    # If VIRTUAL_ENV_DISABLE_PROMPT is set, then reset to old prompt
+    if ! [ -z "${VIRTUAL_ENV_DISABLE_PROMPT+_}" ]; then
+        PS1="$_OLD_VIRTUAL_PS1"
+        return
+    fi
+
+    local prompt_python_msg
+    local python_exe
+
+    # Exit code will determine whether python executable exists or not
+    python_exe=$(which python)
+
+    if [ $? -ne 0 ]; then
+        prompt_python_msg="Error! Python not found"
+    elif ! [[ "$python_exe" == "$VIRTUAL_ENV"* ]]; then
+        prompt_python_msg="Error! Python executable isn't in VIRTUAL_VENV"
+    else
+        # Get Python version from the virtual environment
+        local python_version=$(python --version 2>&1 | cut -d' ' -f2)
+        python_version=${python_version//$'\r'/} # Remove carriage return
+
+        # Extract the environment name without parentheses and create a new prompt
+        local venv_name=${VIRTUAL_ENV_PROMPT//[()]/}
+        venv_name=${venv_name%% *} # Remove any trailing spaces
+
+        prompt_python_msg="$venv_name $python_version"
+    fi
+
+    local python_icon
+    if ! [ -z ${OMZ_PYTHON_ICON+_} ]; then
+        python_icon="$OMZ_PYTHON_ICON "
+    fi
+    local prompt_prefix="($python_icon$prompt_python_msg) "
+
+    # Set custom prompt
+    PS1="$prompt_prefix${_OLD_VIRTUAL_PS1-}"
 }
 
 # Add our function to precmd hooks to run before each prompt
