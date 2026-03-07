ead407e3d4c44a839ade00af75e8dbb7e3dbb54e
Arthur Nishikawa <nishikawa.poli@gmail.com>
2026-03-07 13:41:50 +0100

    Add option to set python icon

--- plugins/venv-prompt/venv-prompt.plugin.zsh
+++ plugins/venv-prompt/venv-prompt.plugin.zsh
@@ -14,16 +14,16 @@ update_virtual_env_prompt() {
             _OLD_VIRTUAL_PS1="${PS1-}"
         fi
 
-        local prompt_prefix
+        local prompt_python_msg
         local python_exe
 
         # Exit code will determine whether python executable exists or not
         python_exe=$(which python)
-        
+
         if [ $? -ne 0 ]; then
-            prompt_prefix="(Error! Python not found) "
+            prompt_python_msg="Error! Python not found"
         elif ! [[ "$python_exe" == "$VIRTUAL_ENV"* ]]; then
-            prompt_prefix="(Error! Python executable isn't in VIRTUAL_VENV) "
+            prompt_python_msg="Error! Python executable isn't in VIRTUAL_VENV"
         else
             # Get Python version from the virtual environment
             local python_version=$(python --version 2>&1 | cut -d' ' -f2)
@@ -32,8 +32,14 @@ update_virtual_env_prompt() {
             local venv_name=${VIRTUAL_ENV_PROMPT//[()]/}
             venv_name=${venv_name%% *} # Remove any trailing spaces
 
-            prompt_prefix="($venv_name $python_version) "
+            prompt_python_msg="$venv_name $python_version"
         fi
+
+        local python_icon
+        if ! [ -z ${OMZ_PYTHON_ICON+_} ]; then
+            python_icon="$OMZ_PYTHON_ICON "
+        fi
+        local prompt_prefix="($python_icon$prompt_python_msg) "
 
         # Set custom prompt
         PS1="$prompt_prefix${_OLD_VIRTUAL_PS1-}"
